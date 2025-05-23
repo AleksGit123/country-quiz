8b8c8c8d33b97fa0bec089df0a270ecc7be3c0f3
AleksGit123 <aleksichokheli03@gmail.com>
2025-05-24 00:05:38 +0400

    changes

---
+++ script.js
@@ -0,0 +1,126 @@
+let randomBtn = document.querySelector(".random");
+let resetBtn = document.querySelector(".reset");
+let inputs = document.querySelectorAll("input");
+let randomIndex = [];
+let word = "";
+let mixed = "";
+let inputWord = "";
+let guessingWord = document.querySelector(".guessing_word");
+let circleOfTries = document.querySelectorAll(".circles_of_tries");
+let countMistakes = 0;
+function tries(){
+    ++countMistakes;
+   console.log(circleOfTries[countMistakes])
+    for(let i = 0;i < circleOfTries.length;i++){
+        circleOfTries[countMistakes].style.display = "block";
+    }
+}
+
+
+function generateWord(){
+   randomIndex = [];
+    
+    fetch("https://random-word-api.vercel.app/api?words=1&length=6")
+    .then(response =>{
+        return response.json();
+    })
+    .then(data =>{
+        word = data[0];
+        console.log(word)
+
+        // generate unique numbers
+        while (randomIndex.length < 6) {
+            let randomNum = Math.floor(Math.random() * 6);
+            if (!randomIndex.includes(randomNum)) {
+                randomIndex.push(randomNum);
+            }
+        }
+
+        // save mixed words
+        for(let i = 0;i < randomIndex.length;i++){
+            // console.log(randomIndex[i])
+            mixed += data[0][randomIndex[i]];
+        }
+
+        guessingWord.innerHTML = mixed;
+        console.log(mixed);
+    })
+    return word;
+
+}
+
+
+inputs.forEach(input => input.disabled = true)
+
+randomBtn.addEventListener("click", ()=>{
+inputs.forEach(input => input.disabled = false)
+    
+    mixed = "";
+    generateWord();
+
+});
+
+
+inputs.forEach((input,index) =>{
+    input.addEventListener("input", (e)=>{
+    let splited = input.value.split("");
+        console.log(splited)
+
+        // if input word is deleted, input whole word is deleted
+       if( splited.length > 0){
+        inputWord += splited[splited.length-1];
+            // inputWord[inputWord.length - 1] = "";
+        }
+        else if(splited.length < 1){
+            inputWord = inputWord.slice(0,-1);
+            input.style.backgroundColor = "transparent";
+        }
+        else{
+         console.log("error");
+        }
+        console.log(inputWord)
+
+
+        // check if a letter is in the cottect place/
+       if(input.value === ""){
+
+            // input.value = "";
+            input.style.backgroundColor = "transparent";
+            
+        }
+        else if(input.value === word[index]){
+            input.style.backgroundColor = "green";
+        }
+        else{
+            input.style.backgroundColor = "red";
+            tries()
+        
+        }
+
+
+          // check if word is correct
+        if(inputWord === word){
+            console.log("correct");
+            guessingWord.innerHTML = "correct";
+        }
+        else{
+            console.log("incorrect");
+        }
+        
+    })
+    
+    
+})
+
+
+resetBtn.addEventListener("click",() =>{
+    guessingWord.innerHTML = "W O R D";
+    inputs.forEach(input => input.value = "");
+    inputs.forEach(input => input.disabled = true);
+    inputWord = "";
+    mixed = "";
+    word = "";
+    randomIndex = [];
+
+})
+
